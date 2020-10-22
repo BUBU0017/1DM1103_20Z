f5a54937a4816e7c679b65afce1c49520132d15a
BUBU0017 <BUBU0017@wp.pl>
2020-10-22 16:36:21 +0200

    1

---
+++ sem3/z1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[])
+{
+    float a1, a11;
+    a1 = atof(argv[1]);
+    a11 = a1*a1;
+
+    printf("Wartość %s do kwadratu to %f\n ", argv[1], a11);
+
+    return 0;
+}
