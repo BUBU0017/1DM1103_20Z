3a393539c2f611595324066c892b9cd10cec089a
BUBU0017 <73235626+BUBU0017@users.noreply.github.com>
2020-11-20 19:30:35 +0100

    Delete dziekanat.h

--- zad5/dziekanat.h
+++
@@ -1,8 +0,0 @@
-#ifndef _dziekanat_h
-#define _dziekanat_h
-
-void najtrudniejszyPrzedmiot(student dane[100], int ile_rekordow);
-void najlatwiejszyPrzedmiot(student dane[100], int ile_rekordow);
-void najlepszy_student(student dane[100], int ile_rekordow);
-
-#endif
